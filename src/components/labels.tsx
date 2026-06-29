789eda41951c2ba6ec4c95c11cf859201aae334f
sammychiang <sammychiang@sololive000.com>
2026-06-29 23:41:57 +0800

    Add hero section with branding and call-to-action to home page

--- src/components/labels.tsx
+++ src/components/labels.tsx
@@ -1,21 +1,27 @@
+import { twMerge } from "tailwind-merge";
+
 const Color = {
   DarkRed: "#B91C1C",
 } as const;
 
 export function BaseLabel({
   color = "DarkRed",
+  borderRadius,
+  className,
   children,
 }: {
   color?: keyof typeof Color;
+  borderRadius?: React.CSSProperties["borderRadius"];
+  className?: string;
   children: React.ReactNode;
 }) {
   return (
     <div className="flex flex-row items-center gap-2">
       <div
         className="rounded-full px-3 py-1"
-        style={{ backgroundColor: Color[color] }}
+        style={{ backgroundColor: Color[color], borderRadius }}
       >
-        <p className="text-xs text-white">{children}</p>
+        <p className={twMerge("text-xs text-white", className)}>{children}</p>
       </div>
     </div>
   );
--- src/page/home/home.tsx
+++ src/page/home/home.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { postUsers, fetchUsers } from "#/lib/users";
 import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
+import { MdOutlineShield } from "react-icons/md";
+import { BaseLabel } from "#/components/labels";
 
 function Home() {
   const queryClient = useQueryClient();
@@ -37,8 +39,34 @@ function Home() {
   };
 
   return (
-    <div className="page-wrap flex justify-center bg-gray-500 py-6">
-      <div className="min-w-[400px] bg-red-500">home</div>
+    <div className="page-wrap flex justify-center py-6">
+      <div className="min-w-[700px] rounded-2xl bg-[#24140D] px-6 py-6">
+        <div className="flex flex-row items-center gap-2">
+          <MdOutlineShield size={12} color="#C84F53" />
+          <p className="text-sm font-bold text-[#C84F53]">租屋透明化運動</p>
+        </div>
+
+        <div className="mt-4 text-2xl font-bold">
+          <p className="text-white">讓台灣租屋市場</p>
+          <p className="text-[#C84F53]">更透明、更公平</p>
+        </div>
+
+        <div className="mt-4 max-w-[500px]">
+          <p className="text-[14px] text-[#8F8984]">
+            分享你的租屋經驗、揭露惡房東行為、提供市場行情資訊。每一篇文章都讓下一位租客做出更好的決定。
+          </p>
+        </div>
+
+        <div className="mt-4">
+          <BaseLabel
+            className="px-2 py-1 text-[16px] font-bold"
+            color="DarkRed"
+            borderRadius="15px"
+          >
+            加入社群
+          </BaseLabel>
+        </div>
+      </div>
     </div>
   );
 }
